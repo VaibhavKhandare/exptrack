712f3286babd12caf720bc74a047f079fc8a57f7
khandare.v <khandare.v@media.net>
2024-09-11 02:54:13 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.join(__dirname, 'src');
+      return config;
+    },
     env: {
       MONGODB_URI: process.env.MONGODB_URI,
-    },
+    }
 };
 
 export default nextConfig;
+
